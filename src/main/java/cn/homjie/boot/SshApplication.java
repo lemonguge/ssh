90e8657f9e534777fd8554cffb80d9ee08c5a381
HomJie <lemonguge@gmail.com>
2017-07-20 11:52:32 +0800

    1

--- src/main/java/cn/homjie/boot/SshApplication.java
+++ src/main/java/cn/homjie/boot/SshApplication.java
@@ -8,8 +8,9 @@ import com.jcraft.jsch.JSchException;
 public class SshApplication {
 
 	public static void main(String[] args) throws Exception {
-		// upload_6();
-		// upload_8();
+//		 upload_6();
+//		 upload_8();
+		upload_105();
 	}
 
 	static void upload_6() throws JSchException, Exception {
@@ -41,6 +42,28 @@ public class SshApplication {
 		session.upload(FileUtil.REPORT_ZIP, "/usr/local/service");
 		System.out.println("upload ok");
 
+		FileUtil.execFromClasspath("dubbo-mqnotify.sh", session);
+		FileUtil.execFromClasspath("dubbo-quartz.sh", session);
+		FileUtil.execFromClasspath("dubbo-report.sh", session);
+		FileUtil.execFromClasspath("dubbo-transaction-distributed.sh", session);
+	}
+
+	static void upload_105() throws JSchException, Exception {
+		String username = "root";
+		String password = "Fengdai@2017";
+		SSHClient client = new SSHClient("172.30.248.105", username, password);
+		SSHSession session = client.openSession();
+
+		session.upload(FileUtil.PRODUCT_ZIP, "/usr/local/service");
+		session.upload(FileUtil.FINANCE_ZIP, "/usr/local/service");
+		session.upload(FileUtil.MQNOTIFY_ZIP, "/usr/local/service");
+		session.upload(FileUtil.TRANSACTION_DISTRIBUTED_ZIP, "/usr/local/service");
+		session.upload(FileUtil.QUARTZ_ZIP, "/usr/local/service");
+		session.upload(FileUtil.REPORT_ZIP, "/usr/local/service");
+		System.out.println("upload ok");
+
+		FileUtil.execFromClasspath("dubbo-product.sh", session);
+		FileUtil.execFromClasspath("dubbo-finance.sh", session);
 		FileUtil.execFromClasspath("dubbo-mqnotify.sh", session);
 		FileUtil.execFromClasspath("dubbo-quartz.sh", session);
 		FileUtil.execFromClasspath("dubbo-report.sh", session);
